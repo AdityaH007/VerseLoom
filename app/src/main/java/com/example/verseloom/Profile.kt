81e7f51b02854ee6cf3517239fdbe0953e1fa212
AdityaH007 <123583340+AdityaH007@users.noreply.github.com>
2025-02-20 21:11:13 +0530

    Non Essential Updates

--- app/src/main/java/com/example/verseloom/Profile.kt
+++ app/src/main/java/com/example/verseloom/Profile.kt
@@ -1,6 +1,5 @@
 package com.example.verseloom
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
@@ -8,24 +7,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LandingPage : AppCompatActivity() {
+class Profile : AppCompatActivity() {
 
 
-    private lateinit var profileBtn: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_landing_page)
+        setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        profileBtn = findViewById(R.id.Profile)
-        profileBtn.setOnClickListener {
-            val intent = Intent(this, Profile::class.java )
-            startActivity(intent)
-        }
+
     }
 }
